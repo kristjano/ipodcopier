0601da4a4f977e3be560c7ad39a9fd5419918ec1
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2016-05-10 13:10:27 +0000

    Arguments added to packet
    When packet is run it requests argument parameters for source folder and
    target folder.

--- ipodcopier/__main__.py
+++ ipodcopier/__main__.py
@@ -1,5 +1,22 @@
+import argparse
+import os
+import os.path
+
+import stagger
+
+def _parse_arguments():
+    argparser = argparse.ArgumentParser(
+            prog='ipodcopier',
+            description='Copy music files from source to target.')
+    argparser.add_argument('source', metavar='SOURCE_PATH',
+            help='Path to source directory, where the iPod device is mounted.')
+    argparser.add_argument('target', metavar='TARGET_PATH',
+            help='Path to target directory, where the files are to be organized.')
+    return argparser.parse_args()
+
 def main():
-    pass
+    args = _parse_arguments()
 
 if __name__ == '__main__':
-    main
+    main()
+
