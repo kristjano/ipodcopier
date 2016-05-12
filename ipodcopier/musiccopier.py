8f35ce0eea347ed663ec4265eebcbd5b9a2cedb7
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2016-05-12 22:59:45 +0000

    Added some logging
    Logges when copying files and when skipping already existing files.

--- ipodcopier/musiccopier.py
+++ ipodcopier/musiccopier.py
@@ -1,14 +1,24 @@
 import os.path
 import shutil
 import re
+import logging
 
 import stagger
+
+def _setup_logger():
+    '''
+    Format and direct the log to file
+    '''
+    logging.basicConfig(level=logging.INFO,
+            format='%(asctime)s %(levelname)-8s %(message)s',
+            filename='ipodcopier.log',
+            filemode='w')
 
 def _sanitize_filename(filename):
     '''
     Removes characters, that are illegal in path, from string.
     '''
-    sanitize = re.compile(r'[^-_. \w&()]')
+    sanitize = re.compile(r'[^-_. \w&()\']')
     return sanitize.sub('_', filename)
 
 def _organize_info(file_path):
@@ -56,6 +66,7 @@ def copy_music_file(file_path, target):
     '''
     Analizes, organizes and copies single music file from path to target.
     '''
+    logger = logging.getLogger(__name__)
     artist, album, title, track = _organize_info(file_path)
     _, extension = os.path.splitext(os.path.basename(file_path))
     new_dir = os.path.join(target, artist, album)
@@ -69,20 +80,25 @@ def copy_music_file(file_path, target):
                         title=title, extension=extension)
     new_path = os.path.join(new_dir, filename)
     if not os.path.exists(new_path):
+        logger.info('Copying:\n{old} -> {new}'.format(
+                old=file_path, new=new_path))
         return shutil.copy(file_path, new_path)
     else:
-        # log: file already exists, skipping
-        pass
+        logger.info('Skipping (file already exists):\n{old} -> {new}'.format(
+                old=file_path, new=new_path))
 
 def copy_music(source, target):
     '''
     Analizes, organizes and copies all music files contained in the source
     directory to the target directory.
     '''
+    _setup_logger()
     if not os.path.isdir(source):
         print('{source} is not a directory or the directory is not accessable'
                 .format(source=source))
         return
+    print('Copying files...')
     for root, dirs, files in os.walk(source):
         for file in files:
             copy_music_file(os.path.join(root, file), target)
+    print('Done!')
