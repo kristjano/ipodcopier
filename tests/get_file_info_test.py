4830d9eb50df40b5d5696ee91fb4b435c8e6a990
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2016-05-12 01:15:15 +0000

    Added some comments to tests

--- tests/get_file_info_test.py
+++ tests/get_file_info_test.py
@@ -7,13 +7,13 @@ from ipodcopier import musiccopier
 def _test_get_file_info(**expected):
     with tempfile.TemporaryFile() as f:
         # Use dummy file
-        if expected:
+        if expected: # Has ID3 tag
             stagger.util.set_frames(f, expected)
         obtained = musiccopier.get_file_info(f)
     assert obtained == expected, (obtained, expected)
 
 def test():
-    _test_get_file_info()
+    _test_get_file_info() # Test case with file with no ID3 tag
     _test_get_file_info(artist='OMAM', album='', track=0, title='')
     _test_get_file_info(artist='', album='In Rainbows', track=0, title='')
     _test_get_file_info(artist='', album='', track=5, title='')
